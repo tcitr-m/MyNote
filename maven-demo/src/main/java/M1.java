e0e7941c141c3933e745904a6804e3b2d5cf7154
pig <virtoreal@outlook.com>
2022-12-26 11:12:13 +0800

    test yaml

--- maven-demo/src/main/java/M1.java
+++ maven-demo/src/main/java/M1.java
@@ -1,14 +1,20 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import entity.Actress;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Map;
 
 /**
  * @author by PIG
  * @date 2021/3/8 17
  */
 public class M1 {
+
     public static void main(String[] args) {
         ClassLoader classLoader = M1.class.getClassLoader();
         InputStream resourceAsStream = classLoader.getResourceAsStream("data.yml");
@@ -16,8 +22,13 @@ public class M1 {
         System.out.println("resource.getPath() = " + resource.getPath());
         Yaml yaml = new Yaml();
         if (resourceAsStream != null) {
-            Actress actress = yaml.loadAs(resourceAsStream, Actress.class);
-            System.out.println(actress);
-        }
+			Map actress = yaml.loadAs(resourceAsStream, Map.class);
+
+			Gson gson = new Gson();
+			JsonObject ymlObj = gson.toJsonTree(actress).getAsJsonObject();
+			System.out.println(ymlObj);
+			String name = ymlObj.get("name").getAsString();
+			System.out.println(name);
+		}
     }
 }
